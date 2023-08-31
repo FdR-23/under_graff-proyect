79062d94aed12756244e40f22a66b35748ce8298
FeDe <federampi@gmail.com>
2023-08-31 12:53:41 -0300

    ne route navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Navbar from './components/NavBar.jsx'
+import Navbar from '../src/components/NavBar.jsx'
 import Footer from './components/Footer.jsx'
 
 import Home from "./pages/Home"
